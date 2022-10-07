a97908b8c1390e2720c80ad2b3bfd6280de2241b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-10-07 14:06:51 -0700

    start

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import HomeScreen from './HomeScreen';
+import { store } from './store';
 // set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build uber!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
